08c82bf6654d1ec79d2fa897b2e62ac14303743e
York Wang <yw544@nyu.edu>
2018-01-04 18:38:26 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ array.include?(element)
 end
 
 def using_sort(array)
-arrary.sort
+array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+arrary.first
 end
 
 def using_last(array)
-
+arrary.last
 end
 
 def using_size(array)
-
+arrary.size
 end
